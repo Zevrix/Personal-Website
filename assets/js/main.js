b8da829a6f24af6e991d2ad84216146f7e54c39c
Zevrix <arashrai17@gmail.com>
2016-09-17 20:44:52 -0400

    Google analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,11 @@
 				$('#theMenu').toggleClass('menu-open');
 			});
 
+			$('.menu-wrap').click( function() {
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+    	});
+
 
 })(jQuery)
